20908517db25194223d15d6179fc83c46bd63c42
EkeHanson <ekenehanson@gmail.com>
2024-12-02 11:34:54 +0100

    Resolved the 404 not found error

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this matches your Vercel deployment path
   build: {
-    rollupOptions: {
-      input: 'index.html',
-    },
-  },
-  server: {
-    historyApiFallback: true, // Ensures fallback during local development
+    outDir: 'dist', // Default output folder
   },
 });
-
