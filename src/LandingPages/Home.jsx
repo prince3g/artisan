956e989da92d2c5c2d3e658e57409f87855e6f21
EkeHanson <ekenehanson@gmail.com>
2024-12-11 13:09:54 +0100

    Resolved the homepage blank error

--- src/LandingPages/Home.jsx
+++ src/LandingPages/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
       try {
         const response = await fetch(`${djangoHostname}/api/jobs/auth/service-categories/`);
         const data = await response.json();
-        setServices(data.results);
+        setServices(data);
       } catch (error) {
         console.error('Error fetching services:', error);
       } finally {
@@ -146,11 +146,11 @@ function Home() {
         <div className="hero-cont">
           <div className="hero-dlts">
             <div className="hero-dlts-main">
-              <h6>SimserviceHub Trusted Artisan </h6>
+              <h6>SimserviceHub Trusted Tradesperson </h6>
               <h2 className="big-text">
-                Discover Vetted <span>Artisan</span> for Every Home Project
+                Discover Vetted <span>Tradesperson</span> for Every Home Project
               </h2>
-              <p>Your trusted platform for connecting with experienced, reliable Artisans.</p>
+              <p>Your trusted platform for connecting with experienced, reliable Tradespersons.</p>
 
               <div className="Search-Sec">
                 <div className="top-Search">
@@ -219,7 +219,7 @@ function Home() {
                       <label htmlFor="name-input">Search by Name</label>
                       <input
                         type="text"
-                        placeholder="Enter Artisan Name (e.g., John Jerry)"
+                        placeholder="Enter Trade Name (e.g., Electrician)"
                         autoComplete="off"
                         id="name-input"
                         value={inputValue}
@@ -357,19 +357,19 @@ function Home() {
   <div className='Oggg-Grid'>
   <div className='Oggg-Part1'>
     <div className='Oggg-header'>
-  <h2 className='big-text'>Why Choose SimserviceHub’s <span>Artisan</span> Network?</h2>
+  <h2 className='big-text'>Why Choose SimserviceHub’s <span>Tradesperson</span> Network?</h2>
   </div>
   <div className='Oggg-Card'>
     <h3><CheckCircle /> Guaranteed Work Quality</h3>
-    <p>SimserviceHub offers quality assurance for all Artisan projects. T & Cs apply</p>
+    <p>SimserviceHub offers quality assurance for all tradesperson projects. T & Cs apply</p>
   </div>
   <div className='Oggg-Card'>
     <h3><Verified /> Rigorous Screening Process </h3>
-    <p>All Artisan undergo a thorough verification process, including background and skill checks.</p>
+    <p>All tradesperson undergo a thorough verification process, including background and skill checks.</p>
   </div>
   <div className='Oggg-Card'>
-    <h3><People /> Trusted service </h3>
-    <p>We maintain transparency and trust.</p>
+    <h3><People /> Trusted by Thousands </h3>
+    <p>With over 2 million reviews from satisfied clients, we maintain transparency and trust.</p>
   </div>
   </div>
   <div className='Oggg-Part2'>
@@ -384,7 +384,7 @@ function Home() {
 <div className='Cosii-mam'>
   <div className='site-container'>
   <div className='Cosii-mam-header'>
-    <h2 className='mid-text'>How to Hire the Right Artisan with SimserviceHub</h2>
+    <h2 className='mid-text'>How to Hire the Right Tradesperson with SimserviceHub</h2>
   </div>
   <div className='Cosii-mam-Grid'>
   <div className='Cosii-mam-Card'>
@@ -435,7 +435,7 @@ function Home() {
     <img src={HghImg2}></img>
     <div className='hgahgs-Card-Dlt'>
     <h3>Request a Quote</h3>
-    <p>Tell us your requirements, and we’ll connect you with up to three verified Artisan.  </p>
+    <p>Tell us your requirements, and we’ll connect you with up to three verified tradesperson.  </p>
     <Link to="/request-quote" >Request a Quote</Link>
   </div>
   </div>
@@ -444,9 +444,9 @@ function Home() {
   <div className='hgahgs-Card'>
     <img src={HghImg3}></img>
     <div className='hgahgs-Card-Dlt'>
-    <h3>Customers rely on Simservicehub to find skilled, verified trades person.</h3>
-    <p>Sign up and grow your Business Today.</p>
-    <Link to="/artisan-sign-up" >Artisan Sign-Up</Link>
+    <h3>Are You a Tradesperson? Join SimserviceHub Today!</h3>
+    <p>Over 100,000 homeowners rely on SimserviceHub to find skilled, verified tradesperson. Sign up and grow your business today!   </p>
+    <Link to="/tradesperson-sign-up" >Tradesperson Sign-Up</Link>
   </div>
   </div>
 
